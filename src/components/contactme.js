c56aa2bfa13b8021da0da8fba4ab5dbf7b46d0d2
thuy-n3 <ngtan3@gmail.com>
2019-02-13 16:46:08 -0600

    contact me form

--- src/components/contactme.js
+++ src/components/contactme.js
@@ -4,7 +4,12 @@ class ContactMe extends Component {
   render(){
     return(
       <div className="contactMeContainer">
-        <div className=""></div>
+        <form className="contactForm">
+          <input className="name" type="text" placeholder="Your Name"></input>
+          <input className="email" type="email" placeholder="Your Email"></input>
+          <textarea className="message" placeholder="message"></textarea>
+          <input className="submit" type="submit" value="Submit"></input>
+        </form>
       </div>
     )
   }
