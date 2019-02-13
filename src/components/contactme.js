d64637b6f0dadb62a5a4083ecda47b8b6c051175
thuy-n3 <ngtan3@gmail.com>
2019-02-13 16:52:37 -0600

    contact me social

--- src/components/contactme.js
+++ src/components/contactme.js
@@ -10,6 +10,11 @@ class ContactMe extends Component {
           <textarea className="message" placeholder="message"></textarea>
           <input className="submit" type="submit" value="Submit"></input>
         </form>
+        <div className="socialContainer">
+          <a href="/" className="twitter">Twitter</a>
+          <a href="/" className="linkedin">Linkedin</a>
+          <a href="/" className="github">Github</a>
+        </div>
       </div>
     )
   }
