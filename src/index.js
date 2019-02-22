2c2b963ff8551d5352e93612c6a057fb7d1eff72
thuy-n3 <ngtan3@gmail.com>
2019-02-21 21:53:17 -0600

    header done

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ class App extends Component {
                     {/* <HighLevelComponent/> */}
                     
                    <Header />
+                   <AboutMe />
                  
                 </div>
     }
