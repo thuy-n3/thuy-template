f0443af1217cb93e3b350d8ad8b69d7b9b223643
thuy-n3 <ngtan3@gmail.com>
2019-03-01 15:19:07 -0600

    new card

--- src/components/projectcard.js
+++ src/components/projectcard.js
@@ -48,7 +48,127 @@ class ProjectCard extends Component {
                 </div>
               </div>
           </div>
+
+
+
+          <div className="column">
+            <div className="ui fluid card">
+              <div className="image">
+                <img src="./images/coffee/brooke-lark-289769-unsplash.jpg" />
+              </div>
+              <div className="content">
+                <div className="header">Project Title</div>
+                <div className="description">I was very excited. I went through puberty in a theme park. I think American news is pretty tragic in general. </div>
+                <div className="tech">Tech Used: React, Semantic Ui</div>
+              </div>
+              <div className="extra content">
+                <span className="right floated">
+                  <i className="arrow circle right large icon"></i>
+                  <a href="https://github.com" target="blank">Check it out</a>
+                </span>
+                <span>
+                  <i className="github large icon"></i>
+                  <a href="https://github.com" target="blank">Github</a>
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="ui fluid card">
+              <div className="image">
+                <img src="./images/coffee/brooke-lark-289769-unsplash.jpg" />
+              </div>
+              <div className="content">
+                <div className="header">Project Title</div>
+                <div className="description">I was very excited. I went through puberty in a theme park. I think American news is pretty tragic in general. </div>
+                <div className="tech">Tech Used: React, Semantic Ui</div>
+              </div>
+              <div className="extra content">
+                <span className="right floated">
+                  <i className="arrow circle right large icon"></i>
+                  <a href="https://github.com" target="blank">Check it out</a>
+                </span>
+                <span>
+                  <i className="github large icon"></i>
+                  <a href="https://github.com" target="blank">Github</a>
+                </span>
+              </div>
+            </div>
+          </div>
           
+
+
+          <div className="column">
+            <div className="ui fluid card">
+              <div className="image">
+                <img src="./images/coffee/brooke-lark-289769-unsplash.jpg" />
+              </div>
+              <div className="content">
+                <div className="header">Project Title</div>
+                <div className="description">I was very excited. I went through puberty in a theme park. I think American news is pretty tragic in general. </div>
+                <div className="tech">Tech Used: React, Semantic Ui</div>
+              </div>
+              <div className="extra content">
+                <span className="right floated">
+                  <i className="arrow circle right large icon"></i>
+                  <a href="https://github.com" target="blank">Check it out</a>
+                </span>
+                <span>
+                  <i className="github large icon"></i>
+                  <a href="https://github.com" target="blank">Github</a>
+                </span>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div className="column">
+            <div className="ui fluid card">
+              <div className="image">
+                <img src="./images/coffee/brooke-lark-289769-unsplash.jpg" />
+              </div>
+              <div className="content">
+                <div className="header">Project Title</div>
+                <div className="description">I was very excited. I went through puberty in a theme park. I think American news is pretty tragic in general. </div>
+                <div className="tech">Tech Used: React, Semantic Ui</div>
+              </div>
+              <div className="extra content">
+                <span className="right floated">
+                  <i className="arrow circle right large icon"></i>
+                  <a href="https://github.com" target="blank">Check it out</a>
+                </span>
+                <span>
+                  <i className="github large icon"></i>
+                  <a href="https://github.com" target="blank">Github</a>
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="ui fluid card">
+              <div className="image">
+                <img src="./images/coffee/brooke-lark-289769-unsplash.jpg" />
+              </div>
+              <div className="content">
+                <div className="header">Project Title</div>
+                <div className="description">I was very excited. I went through puberty in a theme park. I think American news is pretty tragic in general. </div>
+                <div className="tech">Tech Used: React, Semantic Ui</div>
+              </div>
+              <div className="extra content">
+                <span className="right floated">
+                  <i className="arrow circle right large icon"></i>
+                  <a href="https://github.com" target="blank">Check it out</a>
+                </span>
+                <span>
+                  <i className="github large icon"></i>
+                  <a href="https://github.com" target="blank">Github</a>
+                </span>
+              </div>
+            </div>
+          </div>
 
 
         </div>
